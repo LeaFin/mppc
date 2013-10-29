c0c1d602393ecb5e7e41e979c2b66f7adbda366c
mbrodmann@gmail.com <mbrodmann@gmail.com>
2013-10-29 22:06:36 +0100

    blaaa

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var commandCounter, carryBit, count;
-var commandRegistry = new Array();
+var memory = new Array();
 var register = new Array();
-register[0] = 0;
-register[1] = 0;
-register[2] = 0;
-register[3] = 0;
+register[0] = "0000000000000000";
+register[1] = "0000000000000000";
+register[2] = "0000000000000000";
+register[3] = "0000000000000000";
 
 
 function toBinary(value) {
@@ -12,57 +12,131 @@ function toBinary(value) {
 }
 
 function CLR(index) {
-    register[index] = 0;
+    register[index] = "0000000000000000";
     carryBit = 0;
 }
 
 function ADD(index) {
-    addToAkk(register[index]);
-}
-
-function ADDD(value) {
-    addToAkku(value);
-}
-
-function addToAkku(value) {
-    akku = register[0];
-    register = value;
-    value = akku + register;
-    if (value < -32768 || value > 32767) {
-        carryBit = 1;
-    } else {
-        carryBit = 0;
-        register[0] = value;
+    var akku = register[0];
+    var reg = register[index];
+    var overflow = 0;
+    var result = new Array();
+    for (var i = 15; i >= 0; i--) {
+        switch (parseInt(akku[i]) + parseInt(reg[i]) + overflow) {
+            case 0:
+                result[i] = "0";
+                overflow = 0;
+                break;
+            case 1:
+                result[i] = "1";
+                overflow = 0;
+                break;
+            case 2:
+                result[i] = "0";
+                overflow = 1;
+                break;
+            case 3:
+                result[i] = "1";
+                overflow = 1;
+                break;
+        }
     }
+    register[0] = result.join("");
+    carryBit = overflow;
 }
+
 
 function test() {
-    alert(" ");
-    alert(commandRegistry.join('\n'));
+    DEC();
 }
 
-function loadProgramm(){
-    loadProgrammToCommandRegistry();
-    loadDataToCommandRegistry();
+function loadProgramm() {
+    //loadProgrammandDataToMemory();
     test();
 }
 
-function loadProgrammToCommandRegistry() {
-    var programm = $("#programmInput").val();
-    var lines = programm.replace(/\r\n/g, "\n").split("\n");
-    for (var i = 0; i < lines.length; i++) {
-        var splittedLine = lines[i].split(" ");
-        commandRegistry[splittedLine[0]] = splittedLine[1];
-        commandRegistry[splittedLine[0] + 1] = splittedLine[2];
+function SLA() {
+    var akku = register[0];
+    carryBit = parseInt(akku[0]);
+    akku = akku.substr(1, 15) + "0";
+    register[0] = akku;
+}
+
+function SRA() {
+    var akku = register[0];
+    var msb = akku[0];
+    carryBit = parseInt(akku[15]);
+    akku = msb + akku.substr(0, 15);
+    register[0] = akku;
+}
+
+String.prototype.replaceAt = function(index, character) {
+    return this.substr(0, index) + character + this.substr(index + character.length);
+};
+
+function INC() {
+    var akku = register[0];
+    var last = akku.lastIndexOf("0");
+    if (last === -1) {
+        carryBit = 1;
+        register[0] = "0000000000000000";
+        return;
+    } else {
+        akku = akku.replaceAt(last, "1");
+        var replaceString = "0000000000000000".substr(0, 15 - last);
+        akku = akku.substr(0, last + 1) + replaceString;
+        carryBit = 0;
+        register[0] = akku;
     }
 }
 
-function loadDataToCommandRegistry(){
-    var data = $("#dataInput").val();
-    var lines = data.replace(/\r\n/g, "\n").split("\n");
+function LWDD(index, befehl){
+    register[index] = memory[parseInt(befehl.substring(6, 15), 2)];
+}
+
+function SWDD(index, befehl){
+    memory[parseInt(befehl.substring(6, 15), 2)] = register[index];
+}
+
+function DEC() {
+    var akku = register[0];
+    var dec = "1111111111111111";
+    if (akku === "0000000000000000") {
+        carryBit = 1;
+        register[0] = "1111111111111111";
+        return;
+    } else {
+        var overflow = 0;
+        var result = new Array();
+        for (var i = 15; i >= 0; i--) {
+            switch (parseInt(akku[i]) + parseInt(dec[i]) + overflow) {
+                case 0:
+                    result[i] = "0";
+                    overflow = 0;
+                    break;
+                case 1:
+                    result[i] = "1";
+                    overflow = 0;
+                    break;
+                case 2:
+                    result[i] = "0";
+                    overflow = 1;
+                    break;
+                case 3:
+                    result[i] = "1";
+                    overflow = 1;
+                    break;
+            }
+        }
+        register[0] = result.join("");
+    }
+}
+
+function loadProgrammAndDataToMemory() {
+    var programm = $("#programmAndDataInput").val();
+    var lines = programm.replace(/\r\n/g, "\n").split("\n");
     for (var i = 0; i < lines.length; i++) {
         var splittedLine = lines[i].split(" ");
-        commandRegistry[splittedLine[0]] = splittedLine[1];
-        commandRegistry[splittedLine[0] + 1] = splittedLine[2];
+        memory[splittedLine[0]] = splittedLine[1];
     }
 }
